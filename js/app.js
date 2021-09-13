e80073fbae89b28e201d1bb86b017be13d590300
Md. Asifur Rahman <asifrp6@gmail.com>
2021-09-13 17:57:29 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,85 @@
+const loadProducts = () => {
+  const url = `https://raw.githubusercontent.com/ProgrammingHero1/ranga-store-api/main/ranga-api.json?fbclid=IwAR1DTELeIADY_sF8stE22z6TDBwCv8HYOfeRWhJG3wBRY8C-fAAk-vwTQIE`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => showProducts(data));
+};
+loadProducts();
+
+// show all product in UI 
+const showProducts = (products) => {
+  const allProducts = products.map((pd) => pd);
+  for (const product of allProducts) {
+    const image = product.images;
+    const div = document.createElement("div");
+    div.classList.add("product");
+    div.innerHTML = `<div class="single-product me-md-5 mt-5 mb-3 rounded-3">
+      <div>
+    <img class="product-image" src=${product.image}></img>
+      </div>
+      <h4 class="text-dark mt-3">${product.title}</h4>
+      <p class="text-dark mt-3">Category: ${product.category}</p>
+      <p class="text-dark mt-3">Rating: ${product.rating.rate}</p>
+      <p class="text-dark mt-3">TTotal Public : ${product.rating.count}</p>
+      <h4 class="text-dark mt-3">Price: $ ${product.price}</h4>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success mt-3">add to cart</button>
+      <!-- Button trigger modal -->
+<button type="button" class="btn btn-primary mt-3">Details</button>
+      `;
+    document.getElementById("all-products").appendChild(div);
+  }
+};
+
+let count = 0;
+const addToCart = (id, price) => {
+  count = count + 1;
+  updatePrice("price", price);
+
+  updateTaxAndCharge();
+  document.getElementById("total-Products").innerText = count;
+  updateTotal();
+};
+
+const getInputValue = (id) => {
+  const element = document.getElementById(id).innerText;
+  const converted = parseFloat(element);
+  return converted;
+};
+
+// main price update function
+const updatePrice = (id, value) => {
+  const convertedOldPrice = getInputValue(id);
+  const convertPrice = parseFloat(value);
+  const total = convertedOldPrice + convertPrice;
+  document.getElementById(id).innerText = total.toFixed(2);
+};
+
+// set innerText function
+const setInnerText = (id, value) => {
+  document.getElementById(id).innerText = value.toFixed(2);
+};
+
+// update delivery charge and total Tax
+const updateTaxAndCharge = () => {
+  const priceConverted = getInputValue("price");
+  if (priceConverted > 200) {
+    setInnerText("delivery-charge", 30);
+    setInnerText("total-tax", priceConverted * 0.2);
+  }
+  if (priceConverted > 400) {
+    setInnerText("delivery-charge", 50);
+    setInnerText("total-tax", priceConverted * 0.3);
+  }
+  if (priceConverted > 500) {
+    setInnerText("delivery-charge", 60);
+    setInnerText("total-tax", priceConverted * 0.4);
+  }
+};
+
+//grandTotal update function
+const updateTotal = () => {
+  const grandTotal =
+    getInputValue("price") + getInputValue("delivery-charge") +
+    getInputValue("total-tax");
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
+};
